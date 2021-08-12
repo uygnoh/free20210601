fedf2d83fa571e4e54797b738436e981e1dee4ac
uygnoh <961832097@qq.com>
2021-08-12 19:42:13 +0800

    Thu 12 Aug 2021 07:42:12 PM HKT

--- compiler/python3/learn.py
+++ compiler/python3/learn.py
@@ -125,11 +125,11 @@ for x in range(0, 3):
         print(num_list[x][y])    
     
     
-    
 random_num = random.randrange(0, 100) 
 while (random_num != 15):
     print(random_num)
     random_num = random.randrange(0, 100)
+    
     
 i = 0
 while (i <= 20):
@@ -160,91 +160,6 @@ print("sumNumber : ", addNumber(1, 2))
 ### python3模块 
 ###########################################################
 pandas #科学计算， pip3 install pandas
-
-
-
-###########################################################
-### python3类
-###########################################################
-class Animal:
-    __name   = ""
-    __height = 0
-    __weight = 0
-    __sound  = 0  
-    
-    def __init__(self, name, height, weight, sound):
-        self.__name     = name
-        self.__height   = height
-        self.__weight   = weight
-        self.__sound    = sound        
-    
-    def set_name(self, name):
-        self.__name = name
-    
-    def set_height(self, height):
-        self.__height = height
-        
-    def set_weight(self, weight):
-        self.__weight = weight
-        
-    def set_sound(self, sound):
-        self.__sound = sound
-           
-    def get_name(self):
-        return self.__name
-    
-    def get_height(self):
-        return str(self.__height)
-    
-    def get_weight(self):
-        return str(self.__weight)
-    
-    def get_sound(self):
-        return self.__sound
-    
-    def get_type(self):
-        print("Animal")
-        
-    def toString(self):
-        return "{} is {} cm tall and {} kilograms and say {}".format(self.__name,
-                                                                     self.__height,
-                                                                     self.__weight,
-                                                                     self.__sound)   
-cat = Animal('Whiskers', 33, 10, 'Meow')   
-print(cat.toString())
-
-
-class Dog(Animal):
-    __owner = ""
-    
-    def __init__(self, name, height, weight, sound, owner):
-        self.__owner = owner
-        super(Dog, self).__init__(name, height, weight, sound)
-        
-    def set_owner(self, owner):
-        self.__owner = owner
-        
-    def get_owner(self):
-        return self.__owner
-       
-    def get_type(self):
-        print("Dog")
-    
-    def toString(self):
-        return "{} is {} cm tall and {} kilograms and say {} His owner is {}".format(self.__name,
-                                                                                     self.__height,
-                                                                                     self.__weight,
-                                                                                     self.__sound,
-                                                                                     self.__owner)
-                                                                                     
-    def multiple_sounds(self, how_many=None):
-        if how_many is None:
-            print(self.get_sound())
-        else:
-            print(self.get_sound() * how_many)
-
-spot = Dog("Spot", 53, 27, "Ruff", "Derek")
-print(spot.toString())
 
 
 
