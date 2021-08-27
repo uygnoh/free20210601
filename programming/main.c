644796b6ff450996af12abfbd4306aec7abf1a05
uygnoh <961832097@qq.com>
2021-08-27 11:44:30 +0800

    Fri 27 Aug 2021 11:44:30 AM HKT

--- programming/main.c
+++ programming/main.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
 
-struct book {
-    int a[2];
-};
-typedef struct book * ptrs;
-
-int main(int argc, const char *argv[])
+int main(int argc, const char *argv[]) 
 {
-    ptrs ptr;
-    struct book k;
-    ptr = &k;
-    ptr->a[0] = 321;
-
-    k.a[1] = 123;
-    printf("%d\n", k.a[1]);
-    printf("%d\n", ptr->a[0]);
-	printf("Hello World.");
-	return 0;
+        printf("%d\n", argc);
+        printf("%s\n", argv[0]);
+        return 0;
 }
